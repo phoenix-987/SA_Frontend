9222dce382d79ad1ea79a6f75c704ab73ccb82e2
phoenix <naman.j@consultadd.com>
2024-01-10 15:40:48 -0600

    Implemented Post feature in the app.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,15 @@
+// import AddButton from "./AddButton";
+
 const Header = () => {
     return (
         <div className="header">
             <h1>Student Details</h1>
+            {/* <AddButton /> */}
         </div>
     );
 }
 
 export default Header;
+
+
+
